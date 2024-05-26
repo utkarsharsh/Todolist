0a207b03fdaec87a0e0ff850e711799a85680ada
utkarsharsh <harshupadhyay7786@gmail.com>
2024-05-26 06:24:23 +0530

    some changes

--- Todobackend/index.js
+++ Todobackend/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from 'cors'
+import dotenv from 'dotenv';
 import { Router } from "express";
 import dbconnect from "./config/db.js"
 import routes from "./routes/routes.js"
 const app=express();
+dotenv.config();
 dbconnect();
 app.use(express.json());
 app.use(cors());
 app.use("/api",routes);
-
-app.listen("3000",()=>{
-    console.log("listening");
+const PORT=process.env.PORT;
+app.listen(PORT || "3000",()=>{
+    console.log(PORT);
 })
 
