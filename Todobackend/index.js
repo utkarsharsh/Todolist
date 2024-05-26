cded97ade2ce750b1669b92fe8e66431bc883673
utkarsharsh <harshupadhyay7786@gmail.com>
2024-05-26 06:32:01 +0530

    port

--- Todobackend/index.js
+++ Todobackend/index.js
@@ -10,8 +10,8 @@ dbconnect();
 app.use(express.json());
 app.use(cors());
 app.use("/api",routes);
-const PORT=process.env.PORT;
-app.listen(PORT || "3000",()=>{
-    console.log(PORT);
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(port);
 })
 
